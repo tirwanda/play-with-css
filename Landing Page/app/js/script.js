5adaa1803fd796438417c9c8f9ed0dd59f7df420
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-03-03 13:04:27 +0700

    Add animation on mobile menu

--- Landing Page/app/js/script.js
+++ Landing Page/app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const hasFade = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', () => {
 	if (header.classList.contains('open')) {
+		body.classList.remove('noscroll');
 		header.classList.remove('open');
-		overlay.classList.remove('fade-in');
-		overlay.classList.add('fade-out');
+		hasFade.forEach((elem) => {
+			elem.classList.remove('fade-in');
+			elem.classList.add('fade-out');
+		});
 	} else {
 		header.classList.add('open');
-		overlay.classList.remove('fade-out');
-		overlay.classList.add('fade-in');
+		body.classList.add('noscroll');
+		hasFade.forEach((elem) => {
+			elem.classList.remove('fade-out');
+			elem.classList.add('fade-in');
+		});
 	}
 });
